72e8231b37596365574cd4055d433fa9230af007
parihargaurav <parihargaurav527@gmail.com>
2024-03-30 18:55:46 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,27 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);//-3
+/*
+console.log(2+2) 
+console.log(2**2) 2 raise to power 2*/
+
+let str1 = "Gaurav"
+let str2= " Parihar"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1"+2); // 12   
+console.log(1 + "2"); // 12
+console.log("1" + 2+2); // 122
+console.log(11+2+ "3"); // 133
+console.log("1+2+3"); // 1+2+3
+console.log(+true); // 1
+console.log(+"");// 0
+let gamecounter = 100;
+gamecounter++; // or this way ++gamechanger and will be 101
+console.log(gamecounter);// 101
+
+
+
+
+
