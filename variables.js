7ab6cbe5f0774d63bdd7460db4d195bfd83e2221
parihargaurav <parihargaurav527@gmail.com>
2024-03-30 17:34:19 +0530

    done

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountId = 144533
+let accountEmail = "abc@gmail.com"
+var accountPassword = "1545"
+accountCity = "Jaipur"
+
+// accountId = 23 // not allowed
+
+accountEmail = "gfb@gmail.com"
+accountCity= " Jodhpur"
+accountPassword = "155555"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
