55ed605b5290d238ab7ba803dd30b83bca3ecbf7
parihargaurav <parihargaurav527@gmail.com>
2024-04-01 01:15:58 +0530

    date and time

---
+++ datesTime.js
@@ -0,0 +1,14 @@
+let  myDate = new Date(); // creating objects instance
+console.log(myDate.toDateString());
+// mon apr 01 2024
+
+let myCreaedDate = new Date ("01-14-2025");
+console.log(myCreaedDate.getTime());
+//  date and time are objects
+
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() +1); // as +1 is for months are in staring from 0 here
+console.log(newDate.getDate());
