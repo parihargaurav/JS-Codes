c9e441db478b2da8faade210e447d5cb5258fa83
parihargaurav <parihargaurav527@gmail.com>
2024-04-05 14:47:04 +0530

    done

--- functions.js
+++ functions.js
@@ -27,3 +27,30 @@ console.log(loginUserMeaasage());
 Please enter a username
 undefined
  */
+
+
+// function calculateCartPrice(...num) // here ... is rest or spread operator
+// ... as in rest it will bundle up to a array form  and in spread it spreads the data more cleanly
+
+function calculateCartPrice(val1, val2, ...num1){
+return num1
+}
+
+console.log(calculateCartPrice(200, 500, 2000))
+// val1 is 200 val2 is 500 and then ...num1 will give 2000 as it was left
+
+
+// how to pass object in function
+
+const user = {
+  username: "Gaurav",
+  price: 166
+}
+
+// function definition
+function handleObject(anyobject){
+  console.log(`Username  is ${anyobject.username} and price is ${anyobject.price}`)
+
+}
+// function call 
+handleObject(user ) // Username  is Gaurav and price is 166
