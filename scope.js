2ed889520474bd58032cb1361bc933d752364937
parihargaurav <parihargaurav527@gmail.com>
2024-04-11 15:03:26 +0530

    ok

--- scope.js
+++ scope.js
@@ -1,4 +1,4 @@
-// global scope 
+// global scope
 // values outside block scope is global scope
 let a = 10; //  blocked scope variable let
 const b = 12;
@@ -14,5 +14,18 @@ if (true) {
 // scope ke under value bahar nahi jani chahiye
 console.log(a); // 10
 console.log(c); // 30
-console.log(b);// 12
-// yaha var ne apni value badal li isliye scope varibale ka concept aya hai 
+console.log(b); // 12
+// yaha var ne apni value badal li isliye scope varibale ka concept aya hai
+
+////////***************************/
+
+// Nested function voh hai jo child function apne ke variable ko access kar sakhte hai
+
+if (true) {
+  const username = "Gaurav";
+  if (username === "Gaurav") {
+    const website = " LinkedIn";
+    console.log(username + website); //Gaurav LinkedIn
+
+  }
+}
