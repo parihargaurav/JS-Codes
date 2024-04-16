11db60d2b5d5cd2ae37c388aed7ccdfea57ad583
parihargaurav <parihargaurav527@gmail.com>
2024-04-16 15:28:22 +0530

    theory

---
+++ call_Stack_interview.js
@@ -0,0 +1,20 @@
+//  JavaScript Execution Context 
+// how code will execute
+//  in browser running this keyword value is window object 
+//  as different execution context is created in node browser and other environment
+
+// ? it works on threads
+
+//  whenever the code is run global execution context is created 
+
+//  JAVASCRIPT IS SINGLE THREADED LANGUAGE
+
+/*2 EXECUTION CONTEXT IS CREATE NAME AS 
+    1. Global Execution Context
+    2. Functional Execution Context 
+    
+    2 phases of of code is run
+    1. memory creation phase
+    1.1 execution phase
+    
+    call stack works as lifo based as last in first out  */
