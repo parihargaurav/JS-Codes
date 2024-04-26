d0d333bc2d17f66ea7754e2440e62c0029c7b492
parihargaurav <parihargaurav527@gmail.com>
2024-04-26 22:08:56 +0530

    fetch

--- promises.js
+++ promises.js
@@ -42,8 +42,6 @@ promiseThree.then(function (user) {
   console.log(user);
 });
 
-
-
 // resolve and reject
 
 const promiseFour = new Promise(function (resolve, reject) {
@@ -59,60 +57,67 @@ const promiseFour = new Promise(function (resolve, reject) {
 
 promiseFour
   .then((user) => {
-    console.log(user);   //{ username: 'Chai', email: 'chai@example.com' }
+    console.log(user); //{ username: 'Chai', email: 'chai@example.com' }
 
     return user.username;
   })
   .then((username) => {
-    console.log(username);  // { username: 'Gaurav', password: '123' }
-
+    console.log(username); // { username: 'Gaurav', password: '123' }
   })
   .catch(function (error) {
     console.log(error); // Gaurav
   })
   .finally(() => console.log("The promise is either resolved or rejected"));
 
-
 // new syntax async and await
 // promises is an object.
 
-
-const promiseFive = new Promise(function(resolve, reject){
-    setTimeout(function(){
-        let error = true
-        if (!error) {
-            resolve({username: "javascript", password: "123"})
-        } else {
-            reject('ERROR: JS went wrong')
-        }
-    }, 1000)
+const promiseFive = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    let error = true;
+    if (!error) {
+      resolve({ username: "javascript", password: "123" });
+    } else {
+      reject("ERROR: JS went wrong");
+    }
+  }, 1000);
 });
 
-
-async function consumePromiseFive(){
-    try {
-        const response = await promiseFive
-        console.log(response);
-    } catch (error) {
-        console.log(error); // ERROR: JS went wrong
-
-    }
+async function consumePromiseFive() {
+  try {
+    const response = await promiseFive;
+    console.log(response);
+  } catch (error) {
+    console.log(error); // ERROR: JS went wrong
+  }
 }
 
-consumePromiseFive()
+consumePromiseFive();
 // major problem with async and await is that they can't directly handle the problem
 
+async function getAllUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
 
-
-async function getAllUsers(){
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-        const data = await response.json()
-        console.log(data);
-    } catch (error) {
-        console.log("E: ", error);
-    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("E: ", error);
+  }
 }
 
-getAllUsers()  // dummy api async and await
+getAllUsers(); // dummy api async and await
+
+
+// fetch creates special queue for for operating like itexecutes first when called in call stack. like which ever is in code of execution the fetch api call back will execute first 
+
+fetch("https://api.github.com/users/hiteshchoudhary")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => console.log(error));
+
+// promise.all
