39f0a2bda5789bae7c6cde7e1face3711ca6486c
parihargaurav <parihargaurav527@gmail.com>
2024-04-28 01:28:04 +0530

    oops

---
+++ oops/Objects.js
@@ -0,0 +1,5 @@
+// javascript and classes
+/*  Under the hood, JavaScript's classes are still prototype-based. 
+They provide a more familiar syntax for defining object blueprints and inheritance hierarchies, similar to class-based languages. */
+
+
