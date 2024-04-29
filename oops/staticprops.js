4979fd1b75ba3d50e6b8b63922dec914ce12a5d4
parihargaurav <parihargaurav527@gmail.com>
2024-04-29 19:09:57 +0530

    inheritance_static

---
+++ oops/staticprops.js
@@ -0,0 +1,34 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId());
+
+
+/*
+
+Static class methods are defined on the class itself.
+
+You cannot call a static method on an object, only on an object class.
+ */
